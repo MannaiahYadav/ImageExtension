53144fc8ef3a35c63d56664cf356612ab0540661
Mannaiah <mannaiah.01@karvy.com>
2019-09-26 15:03:38 +0530

    newCommit

---
+++ Example/Pods/Target Support Files/ImageExtension/ImageExtension-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ImageExtension : NSObject
+@end
+@implementation PodsDummy_ImageExtension
+@end
---
+++ Example/Pods/Target Support Files/Pods-ImageExtension_Example/Pods-ImageExtension_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ImageExtension_Example : NSObject
+@end
+@implementation PodsDummy_Pods_ImageExtension_Example
+@end
